dc564c0ebfdaffd7d2239d9664f396654bfff2c8
Adrian Cooke <adriangerardcooke+theonly@gmail.com>
2011-01-09 15:18:24 +0200

    Made sure that the index page also gets accessed.

--- generic_content_context_processors.py
+++ generic_content_context_processors.py
@@ -1,7 +1,11 @@
 from generic_content.models import GenericContent
 def generic_content(request):
     try:
-        generic_content = GenericContent.objects.get(def_url=request.path);
+        if request.path == '':
+            path = '/'
+        else:
+            path = request.path
+        generic_content = GenericContent.objects.get(def_url=path);
     except GenericContent.DoesNotExist:
         generic_content = None
     return {'generic_content':generic_content}
